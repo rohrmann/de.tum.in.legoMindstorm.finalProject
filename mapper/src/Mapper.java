c20de198a790feb2608ce76bb878db53432fa477
unknown <Butterfly@.(none)>
2010-09-20 11:20:59 +0200

    Config updated

--- mapper/src/Mapper.java
+++ mapper/src/Mapper.java
@@ -45,7 +45,7 @@ public class Mapper {
 		LightSensor rightLightSensor = new LightSensor(SensorPort.S2);		
 		Color[] colors = {Color.WHITE,Color.BLACK,Color.BLUE,Color.RED,Color.GREEN,Color.YELLOW};
 		ColorSettings color = new ColorSettings(colorSensor);
-		color.init(colors,Config.times,Config.mapperPollingInterval);
+		color.init(colors,Config.colorScanTimes,Config.mapperPollingInterval);
 		LightSettings leftLightSettings = new LightSettings(leftLightSensor);
 		LightSettings rightLightSettings = new LightSettings(rightLightSensor);
 		int tolerance =5;
