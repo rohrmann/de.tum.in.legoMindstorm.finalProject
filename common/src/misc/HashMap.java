44150e8d615126dab02a57f359f94889edca67ba
Andi <p038@atradig61.informatik.tu-muenchen.de>
2010-09-21 12:25:48 +0200

    HashMap-Prototyp

--- common/src/misc/HashMap.java
+++ common/src/misc/HashMap.java
@@ -1,34 +1,100 @@
 package misc;
 
 import Graph.Pair;
+
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Vector;
+
+import sun.security.krb5.internal.crypto.e;
 
 public class HashMap<T> {
 
-	Hashtable hashtable;
+	Vector keys;
+	Vector vals;
 	
 	public HashMap()
 	{
-		hashtable = new Hashtable();
+		keys = new Vector();
+		vals = new Vector();
 	}
 	
 	public void put(Pair pair, T i)
 	{
-		hashtable.put(pair, i);
+		keys.addElement(pair);
+		vals.addElement(i);
 	}
 	
-	public void remove(Pair pair)
+	public boolean containsKey(Pair p)
 	{
-		hashtable.put(pair, null);
+		return keys.contains(p);
 	}
 	
-	public boolean containsKey(Pair pair)
+	public void remove(Pair p)
 	{
-		return hashtable.get(pair) != null;
+		int i = keys.indexOf(p);
+		vals.removeElementAt(i);
+		keys.removeElementAt(i);
 	}
 	
-	public T get(Pair pair)
+	public T get(Pair p)
 	{
-		return (T)hashtable.get(pair);
+		int i = keys.indexOf(p);
+		return (T)vals.elementAt(i);
 	}
+	
+//	Hashtable hashtable;
+//	
+//	private int length = 14;
+//	private ArrayList<HashMapElement<T>>[] data;
+//	
+//	public HashMap()
+//	{
+//		data = new ArrayList[length];
+//		for(int i=0; i<data.length; i++)
+//			data[i] = new ArrayList<HashMapElement<T>>();
+//	}
+//	
+//	public void put(Pair pair, T i)
+//	{
+//		int h = hash(pair);
+//		if(!containsKey(pair))
+//		{
+//			data[hash(pair)].add(new HashMapElement<T>(pair, i));
+//		}
+//			
+//	}
+//	
+//	public void remove(Pair pair)
+//	{
+//		int h = hash(pair);
+//		
+//		Iterator<>
+//		data[h].add(new HashMapElement<T>(pair, i));
+//	}
+//	
+//	public boolean containsKey(Pair pair)
+//	{
+//		return hashtable.get(pair) != null;
+//	}
+//	
+//	public T get(Pair pair)
+//	{
+//		return (T)hashtable.get(pair);
+//	}
+//	
+//	private int hash(Pair pair)
+//	{
+//		return (pair.getX() + (pair.getY()/3 * 7)) % length;
+//	}
+//	
+//	private class HashMapElement<T>
+//	{
+//		public Pair key;
+//		public T val;
+//		public HashMapElement(Pair key, T val){
+//			this.key = key;
+//			this.val = val;
+//		}
+//	}
 }
