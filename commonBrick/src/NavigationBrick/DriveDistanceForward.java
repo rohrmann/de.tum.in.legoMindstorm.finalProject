c2f82794f4d858be96c08be99a6aacd826e4690d
Andi <p038@atradig61.informatik.tu-muenchen.de>
2010-09-22 16:12:18 +0200

    DriveDistanceForward.java

---
+++ commonBrick/src/NavigationBrick/DriveDistanceForward.java
@@ -0,0 +1,56 @@
+package NavigationBrick;
+
+import lejos.robotics.subsumption.Behavior;
+import miscBrick.Robot;
+
+
+public class DriveDistanceForward implements Behavior{
+
+	Robot robot;
+	float distance;
+	boolean active = false;
+	
+	public DriveDistanceForward(Robot robot, float distance)
+	{
+		this.distance = distance;
+		this.robot = robot;
+		robot.getPilot().reset();
+	}
+	
+	public void setDistance(float distance)
+	{
+		this.distance = distance;
+		robot.getPilot().reset();
+	}
+	
+	public void action() {
+		//robot.getPilot().travel(distance, false);
+		robot.getPilot().forward();
+		
+		while(active && distance > robot.getPilot().getTravelDistance())
+		{
+			//if(!robot.getPilot().isMoving()) robot.getPilot().forward();
+		}
+		
+		suppress();
+		
+	}
+
+	public void suppress() {
+		active = false;
+		robot.getPilot().stop();
+		distance -= robot.getPilot().getTravelDistance();
+		robot.getPilot().reset();
+	}
+
+	public boolean takeControl() {
+		if(distance>0)
+		{
+			active = true;
+			return true;
+		}
+		return false;
+		
+	}
+
+}
